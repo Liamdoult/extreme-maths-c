0b4a84c419b7a9364a2a2ca0af2840dd72eef563
LiamDoult <3579535@myuwc.ac.za>
2020-12-01 11:31:37 +0100

    Multithreaded c support for existing functions
    Add support CPU multi-threading to optimise performance across CPU bound
    devices.
    
    Computation has thread creation overhead and therefore, it may still be quicker
    for smaller data amounts to be executed on the same thread.

--- Makefile
+++ Makefile
@@ -1,16 +1,19 @@
 all:
-	nvcc --compiler-options '-fPIC -O3' --shared -I include src/vector.c -o ./bin/libem.so
+	gcc -O3 -fPIC --shared -I include src/vector.c -o ./bin/libem.so
 	nvcc --compiler-options '-fPIC -O3' --shared -I include src/vector.cu -o ./bin/libem_cuda.so
 	gcc -O3 -fPIC --shared -I include src/vector_cl.c -o ./bin/libem_ocl.so -lOpenCL -lm
+	gcc -O3 -fPIC --shared -I include src/vector_threaded.c -o ./bin/libem_threaded.so
 
 open_cl_preprocessing:
 	touch include/ocl_kernal_source.h
 	bash stringify_ocl src/vector.cl
 
 test: open_cl_preprocessing
-	nvcc -I include src/vector.c src/test.c -o ./build/c_test
+	gcc -I include src/vector.c src/test.c -o ./build/c_test
 	@./build/c_test
 	nvcc -I include src/vector.cu src/test.c -o ./build/cu_test
 	@./build/cu_test
 	gcc -I include src/vector_cl.c src/test.c -o ./build/ocl_test -lOpenCL -lm
 	@./build/ocl_test
+	gcc -I include src/vector_threaded.c src/test.c -o ./build/threaded_test -pthread
+	@./build/threaded_test
--- src/vector_threaded.c
+++ src/vector_threaded.c
@@ -4,8 +4,9 @@
 
 #include <pthread.h>
 
-#include "vector_add.h"
+#include "extreme_maths.h"
 
+int THREADS =  8;
 
 struct ThreadArgs {
     float *out;
@@ -15,28 +16,12 @@ struct ThreadArgs {
     int i;
 };
 
-void *_vector_add(void *ptr) {
-    struct ThreadArgs *args = (struct ThreadArgs *)ptr;
-    float *out, *a, *b;
-    int s, e, c, i;
-
-    out = args->out;
-    a = args->a;
-    b = args->b;
-    c = args->c;
-    i = args->i;
-
-    s = i * c;
-    e = (i + 1) * c;
-    
-    // printf("i:  %d\n", i);
-    // printf("Start:  %d\n", s);
-    // printf("End: %d\n", e);
-
-    for(int i = s; i < e; i++){
-        out[i] = a[i] + b[i];
-    }
-}
+struct ThreadiArgs {
+    float *a;
+    float *b;
+    int c;
+    int i;
+};
 
 struct Vector create_vector(float *a, int size) {
     float *array; 
@@ -51,30 +36,368 @@ struct Vector create_vector(float *a, int size) {
     return vec;
 }
 
-float* get_result(struct Vector a) {
-    return a.array;
+struct Vector generate_vector(int size) {
+    float *array; 
+
+    array = (float*)malloc(sizeof(float) * size);
+
+    struct Vector vec;
+    vec.size = size;
+    vec.array = array;
+    return vec;
 }
 
-void vector_add(struct Vector a, struct Vector b, struct Vector res) {
-    int THREADS =  8;
-    // printf("THREADS %d\n", THREADS);
-    int chunk_size = a.size/THREADS;
-    // printf("chunk size: %d\n", chunk_size);
+float* get_result(struct Vector *a) {
+    return a->array;
+}
 
-    pthread_t threads[THREADS];
+void init() {};
+void clean() {};
+
+void clean_vector(struct Vector a) {
+    free(a.array);
+}
+
+void *_vector_add(void *ptr) {
+    struct ThreadArgs *args = (struct ThreadArgs *)ptr;
+    float *out, *a, *b;
+    int s, e, c, i;
+
+    out = args->out;
+    a = args->a;
+    b = args->b;
+    c = args->c;
+    i = args->i;
+
+    s = i * c;
+    e = (i + 1) * c;
+
+    for(int i = s; i < e; i++){
+        out[i] = a[i] + b[i];
+    }
+}
+
+void *_vector_iadd(void *ptr) {
+    struct ThreadiArgs *args = (struct ThreadiArgs *)ptr;
+    float *a, *b;
+    int s, e, c, i;
+
+    a = args->a;
+    b = args->b;
+    c = args->c;
+    i = args->i;
+
+    s = i * c;
+    e = (i + 1) * c;
+
+    for(int i = s; i < e; i++){
+        a[i] += b[i];
+    }
+}
+
+struct Vector vector_add(struct Vector *a, struct Vector *b) {
+    struct Vector res = generate_vector(a->size);
+
+    int _threads = THREADS < a->size? THREADS : a->size;
+
+    int chunk_size = a->size/_threads;
+
+    pthread_t threads[_threads];
     
-    struct ThreadArgs vas[THREADS];
+    struct ThreadArgs vas[_threads];
 
     int i;
-    for (i = 0; i < THREADS; i++) {
+    for (i = 0; i < _threads; i++) {
         vas[i].out = res.array;
-        vas[i].a = a.array;
-        vas[i].b = b.array;
+        vas[i].a = a->array;
+        vas[i].b = b->array;
         vas[i].c = chunk_size;
         vas[i].i = i;
         pthread_create(&threads[i], NULL, _vector_add, (void *)&vas[i]); 
     }
-    for (i = 0; i < THREADS; i++) {
+    for (i = 0; i < _threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    return res;
+}
+
+void vector_iadd(struct Vector *a, struct Vector *b) {
+    int _threads = THREADS < a->size? THREADS : a->size;
+
+    int chunk_size = a->size/_threads;
+
+    pthread_t threads[_threads];
+    
+    struct ThreadiArgs vas[_threads];
+
+    int i;
+    for (i = 0; i < _threads; i++) {
+        vas[i].a = a->array;
+        vas[i].b = b->array;
+        vas[i].c = chunk_size;
+        vas[i].i = i;
+        pthread_create(&threads[i], NULL, _vector_iadd, (void *)&vas[i]); 
+    }
+    for (i = 0; i < _threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+}
+
+void *_vector_sub(void *ptr) {
+    struct ThreadArgs *args = (struct ThreadArgs *)ptr;
+    float *out, *a, *b;
+    int s, e, c, i;
+
+    out = args->out;
+    a = args->a;
+    b = args->b;
+    c = args->c;
+    i = args->i;
+
+    s = i * c;
+    e = (i + 1) * c;
+
+    for(int i = s; i < e; i++){
+        out[i] = a[i] - b[i];
+    }
+}
+
+void *_vector_isub(void *ptr) {
+    struct ThreadiArgs *args = (struct ThreadiArgs *)ptr;
+    float *a, *b;
+    int s, e, c, i;
+
+    a = args->a;
+    b = args->b;
+    c = args->c;
+    i = args->i;
+
+    s = i * c;
+    e = (i + 1) * c;
+
+    for(int i = s; i < e; i++){
+        a[i] -= b[i];
+    }
+}
+
+struct Vector vector_sub(struct Vector *a, struct Vector *b) {
+    struct Vector res = generate_vector(a->size);
+
+    int _threads = THREADS < a->size? THREADS : a->size;
+
+    int chunk_size = a->size/_threads;
+
+    pthread_t threads[_threads];
+    
+    struct ThreadArgs vas[_threads];
+
+    int i;
+    for (i = 0; i < _threads; i++) {
+        vas[i].out = res.array;
+        vas[i].a = a->array;
+        vas[i].b = b->array;
+        vas[i].c = chunk_size;
+        vas[i].i = i;
+        pthread_create(&threads[i], NULL, _vector_sub, (void *)&vas[i]); 
+    }
+    for (i = 0; i < _threads; i++) {
+        int rval = pthread_join(threads[i], NULL);
+    }
+
+    return res;
+}
+
+void vector_isub(struct Vector *a, struct Vector *b) {
+    int _threads = THREADS < a->size? THREADS : a->size;
+
+    int chunk_size = a->size/_threads;
+
+    pthread_t threads[_threads];
+    
+    struct ThreadiArgs vas[_threads];
+
+    int i;
+    for (i = 0; i < _threads; i++) {
+        vas[i].a = a->array;
+        vas[i].b = b->array;
+        vas[i].c = chunk_size;
+        vas[i].i = i;
+        pthread_create(&threads[i], NULL, _vector_isub, (void *)&vas[i]); 
+    }
+    for (i = 0; i < _threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+}
+
+void *_vector_mul(void *ptr) {
+    struct ThreadArgs *args = (struct ThreadArgs *)ptr;
+    float *out, *a, *b;
+    int s, e, c, i;
+
+    out = args->out;
+    a = args->a;
+    b = args->b;
+    c = args->c;
+    i = args->i;
+
+    s = i * c;
+    e = (i + 1) * c;
+
+    for(int i = s; i < e; i++){
+        out[i] = a[i] * b[i];
+    }
+}
+
+void *_vector_imul(void *ptr) {
+    struct ThreadiArgs *args = (struct ThreadiArgs *)ptr;
+    float *a, *b;
+    int s, e, c, i;
+
+    a = args->a;
+    b = args->b;
+    c = args->c;
+    i = args->i;
+
+    s = i * c;
+    e = (i + 1) * c;
+
+    for(int i = s; i < e; i++){
+        a[i] *= b[i];
+    }
+}
+
+struct Vector vector_mul(struct Vector *a, struct Vector *b) {
+    struct Vector res = generate_vector(a->size);
+
+    int _threads = THREADS < a->size? THREADS : a->size;
+
+    int chunk_size = a->size/_threads;
+
+    pthread_t threads[_threads];
+    
+    struct ThreadArgs vas[_threads];
+
+    int i;
+    for (i = 0; i < _threads; i++) {
+        vas[i].out = res.array;
+        vas[i].a = a->array;
+        vas[i].b = b->array;
+        vas[i].c = chunk_size;
+        vas[i].i = i;
+        pthread_create(&threads[i], NULL, _vector_mul, (void *)&vas[i]); 
+    }
+    for (i = 0; i < _threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    return res;
+}
+
+void vector_imul(struct Vector *a, struct Vector *b) {
+    int _threads = THREADS < a->size? THREADS : a->size;
+
+    int chunk_size = a->size/_threads;
+
+    pthread_t threads[_threads];
+    
+    struct ThreadiArgs vas[_threads];
+
+    int i;
+    for (i = 0; i < _threads; i++) {
+        vas[i].a = a->array;
+        vas[i].b = b->array;
+        vas[i].c = chunk_size;
+        vas[i].i = i;
+        pthread_create(&threads[i], NULL, _vector_imul, (void *)&vas[i]); 
+    }
+    for (i = 0; i < _threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+}
+
+void *_vector_div(void *ptr) {
+    struct ThreadArgs *args = (struct ThreadArgs *)ptr;
+    float *out, *a, *b;
+    int s, e, c, i;
+
+    out = args->out;
+    a = args->a;
+    b = args->b;
+    c = args->c;
+    i = args->i;
+
+    s = i * c;
+    e = (i + 1) * c;
+
+    for(int i = s; i < e; i++){
+        out[i] = a[i] / b[i];
+    }
+}
+
+void *_vector_idiv(void *ptr) {
+    struct ThreadiArgs *args = (struct ThreadiArgs *)ptr;
+    float *a, *b;
+    int s, e, c, i;
+
+    a = args->a;
+    b = args->b;
+    c = args->c;
+    i = args->i;
+
+    s = i * c;
+    e = (i + 1) * c;
+
+    for(int i = s; i < e; i++){
+        a[i] /= b[i];
+    }
+}
+
+struct Vector vector_div(struct Vector *a, struct Vector *b) {
+    struct Vector res = generate_vector(a->size);
+
+    int _threads = THREADS < a->size? THREADS : a->size;
+    
+    int chunk_size = a->size/_threads;
+
+    pthread_t threads[_threads];
+    
+    struct ThreadArgs vas[_threads];
+
+    int i;
+    for (i = 0; i < _threads; i++) {
+        vas[i].out = res.array;
+        vas[i].a = a->array;
+        vas[i].b = b->array;
+        vas[i].c = chunk_size;
+        vas[i].i = i;
+        pthread_create(&threads[i], NULL, _vector_div, (void *)&vas[i]); 
+    }
+    for (i = 0; i < _threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    return res;
+}
+
+void vector_idiv(struct Vector *a, struct Vector *b) {
+    int _threads = THREADS < a->size? THREADS : a->size;
+
+    int chunk_size = a->size/_threads;
+
+    pthread_t threads[_threads];
+    
+    struct ThreadiArgs vas[_threads];
+
+    int i;
+    for (i = 0; i < _threads; i++) {
+        vas[i].a = a->array;
+        vas[i].b = b->array;
+        vas[i].c = chunk_size;
+        vas[i].i = i;
+        pthread_create(&threads[i], NULL, _vector_idiv, (void *)&vas[i]); 
+    }
+    for (i = 0; i < _threads; i++) {
         pthread_join(threads[i], NULL);
     }
 }
